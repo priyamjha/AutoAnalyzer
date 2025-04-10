7b6878dd59436e23fdc93628baa3d43aca0a30d3
Priyam Jha <priyamj584@gmail.com>
2025-04-10 19:25:52 +0530

    first commit

--- analyzer/templatetags/custom_filters.py
+++ analyzer/templatetags/custom_filters.py
@@ -1,7 +1,24 @@
 from django import template
+
+# Register a new template filter library so Django knows where to find custom filters
 register = template.Library()
 
 @register.filter
 def get_item(dictionary, key):
-    return dictionary.get(key)
+    """
+    Custom template filter to retrieve a value from a dictionary using a key.
+    
+    Usage in Django templates:
+        {{ my_dict|get_item:"my_key" }}
+    
+    Why it's needed:
+        - Django templates don't support dictionary-style access like my_dict["my_key"]
+        - This filter allows you to dynamically access dictionary values when the key is a variable
+        - Especially useful when looping over data and needing to look up related values
 
+    Example:
+        {% for col, val in stats.most_freq.items %}
+            {{ stats.freq_percent|get_item:col }}%
+        {% endfor %}
+    """
+    return dictionary.get(key)
